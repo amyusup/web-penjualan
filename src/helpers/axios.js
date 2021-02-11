6a3e8f908208b473416b7e40be611ccf0b7f3616
amyusup <amyusup26@gmail.com>
2021-02-11 15:34:31 +0700

    setup hosting

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "http://54.86.186.28:4444/api/v1",
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://penjualan-api.herokuapp.com/api/v1",
+  // baseURL: "http://localhost:8000/api/v1",
 });
